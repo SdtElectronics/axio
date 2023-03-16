81ece9cc8ed895ebe5de632be7ce0608a4c51b5b
SdtElectronics <meiyang423@Gmail.com>
2023-03-15 23:14:36 -0500

    add: support pipe
    * add pipe.h
    * add pipe.cpp

---
+++ include/channel.h
@@ -0,0 +1,50 @@
+#ifndef AXIO_PIPE
+#define AXIO_PIPE
+
+#include <cerrno>
+
+#include <poll.h>
+
+#include "./emitter.h"
+#include "./write.h"
+
+namespace axio {
+    class Channel{
+      public:
+        Channel(int flags);
+        int fds[2];
+    };
+
+    class ChanTX: public Emitter {
+      public:
+        enum Events: EvtVec { writable = POLLOUT };
+        enum InitErr { ioError = EIO };
+
+        using Emitter::Emitter;
+
+        ChanTX(int fd, int flags = 0);
+
+        int send(std::string_view buffer) const noexcept;
+        int send(std::initializer_list<Iov> svs) const noexcept;
+
+        static int construct(Channel chan) noexcept;
+        static int construct(int fd, int flags);
+
+        static constexpr EvtVec defEvts = writable;
+    };
+
+    class ChanRX: public Emitter {
+      public:
+        enum Events: EvtVec { readable = POLLIN };
+        enum InitErr { ioError = EIO };
+
+        using Emitter::Emitter;
+
+        static int construct(Channel chan) noexcept;
+        static int construct(int fd, int flags = 0);
+
+        static constexpr EvtVec defEvts = readable;
+    };
+} // namespace axio
+
+#endif // AXIO_PIPE
